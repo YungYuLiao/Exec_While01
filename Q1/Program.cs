6167fde6f8f7c5b5ce82e61848c5b009beb88822
User <User@DESKTOP-QVNS494>
2022-10-22 17:53:44 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//1+ 2 + 3 + 4 + .... + N <105 請問 N 是多少 ?
+			int current = 0;
+			int restriction = 105;
+			int count = 0;
+			while (count < restriction)
+			{
+				current += 1;
+				count += current;
+			}
+			Console.WriteLine(current-1); //while 加N大於105的狀況時會停，若要小於105的值則為N-1
+
+
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//使用 while , 在畫面顯示所有可以整除 300 的整數例如: 1, 2, 3, 4, 5, 6, 10, ...., 300
+			int current = 1;
+			int restriction = 300;
+			
+			while (current <= restriction)
+			{
+				current++;
+				if (restriction % current == 0)
+				{
+					Console.WriteLine(current);
+				}
+				continue;
+			}
+		}
+	}
+}
